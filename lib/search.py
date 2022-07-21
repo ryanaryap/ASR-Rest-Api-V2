c0c098a304636c87590a3e126105409617c05207
Ryan Arya Pramudya <93182970+ryanaryap@users.noreply.github.com>
2022-07-22 04:06:36 +0700

    Update search.py

--- lib/search.py
+++ lib/search.py
@@ -17,3 +17,7 @@ def search_otakudesu(query):
 def search_lk21(query):
 	url = lk21 = bs(get('https://lk21.asia/?s=%s#gsc.tab=0&gsc.q=resident&gsc.page=1' % query, headers=usr_agent).text, 'html.parser').find('div', class_='search-wrapper').a['href']
 	return url
+
+def search_adkflm(query):
+	url = adkflm = bs(get('https://adikfilm.cfd/?s=%s' % query, headers=usr_agent).text, 'html.parser').find('div', class_='gmr-watch-movie').a['href']
+	return url
