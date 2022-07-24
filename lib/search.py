c01ad69cbecfa68b089c437a9ad64b90c861045a
Ryan Arya Pramudya <93182970+ryanaryap@users.noreply.github.com>
2022-07-24 19:30:27 +0700

    Update search.py

--- lib/search.py
+++ lib/search.py
@@ -1,5 +1,5 @@
-from requests import get
 from bs4 import BeautifulSoup as bs
+import requests
 import random
 
 ua = ['Mozilla/5.0 (X11; CrOS x86_64 13310.76.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.108 Safari/537.36','Mozilla/5.0 (X11; CrOS x86_64 11895.118.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.159 Safari/537.36','Mozilla/5.0 (X11; CrOS x86_64 12239.92.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.136 Safari/537.36','Mozilla/5.0 (X11; CrOS x86_64 13099.110.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.136 Safari/537.36','Mozilla/5.0 (X11; CrOS x86_64 13099.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.127 Safari/537.36','Mozilla/5.0 (X11; CrOS x86_64 13020.87.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.119 Safari/537.36','Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36','Mozilla/5.0 (X11; CrOS x86_64 12499.66.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.106 Safari/537.36','Mozilla/5.0 (X11; CrOS x86_64 13310.59.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.84 Safari/537.36','Mozilla/5.0 (X11; CrOS x86_64 12739.111.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Safari/537.36','Mozilla/5.0 (X11; CrOS x86_64 12607.82.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.123 Safari/537.36','Mozilla/5.0 (X11; CrOS x86_64 13099.85.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.110 Safari/537.36','Mozilla/5.0 (X11; CrOS x86_64 12607.58.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.86 Safari/537.36']
@@ -7,17 +7,17 @@ usr_agent = {
 	'User-Agent': random.choice(ua)
 	}
 def search_kusonime(query):
-    url = bs(get('https://kusonime.com/?s=%s' % query, headers=usr_agent).text, 'html.parser').find('div', class_='detpost').a['href']
+    url = bs(requests.get('https://kusonime.com/?s=%s' % query, headers=usr_agent).text, 'html.parser').find('div', class_='detpost').a['href']
     return url
     
 def search_otakudesu(query):
-	url = otakudesu = bs(get('https://otakudesu.watch/?s=%s&post_type=anime' % query, headers=usr_agent).text, 'html.parser').find('ul', class_='chivsrc').a['href']
+	url = otakudesu = bs(requests.get('https://otakudesu.watch/?s=%s&post_type=anime' % query, headers=usr_agent).text, 'html.parser').find('ul', class_='chivsrc').a['href']
 	return url
 
 def search_lk21(query):
-	url = lk21 = bs(get('https://lk21.asia/?s=%s#gsc.tab=0&gsc.q=resident&gsc.page=1' % query, headers=usr_agent).text, 'html.parser').find('div', class_='search-content').a['href']
+	url = lk21 = bs(requests.get('https://lk21.asia/?s=%s' % query, headers=usr_agent).text, 'html.parser').find('div', class_='search-content').a['href']
 	return url
 
 def search_adkflm(query):
-	url = adkflm = bs(get('https://adikfilm.cfd/?s=%s' % query, headers=usr_agent).text, 'html.parser').find('div', class_='gmr-watch-movie').a['href']
+	url = adkflm = bs(requests.get('https://adikfilm.cfd/?s=%s' % query, headers=usr_agent).text, 'html.parser').find('div', class_='gmr-watch-movie').a['href']
 	return url
